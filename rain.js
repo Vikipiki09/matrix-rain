ff4cb315a25a1bf75ba3d1d5dde1e3cf5eadf031
Viktor <viki.alxndrv1@gmail.com>
2023-01-22 11:05:13 -0600

    matrix rain

--- rain.js
+++ rain.js
@@ -22,9 +22,24 @@ setInterval(drow, 100);
 
 function randomMatrixSymbol() {
     const chars = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ";
+    function drow() {
+        y += 50;
+        blackTush()
+        writeASymbol();
+    }
+    function blackTush() {
+        ctx.fillStyle = "rgba(0,0,0,0.1"
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+        function writeASymbol() {
+            ctx.fillStyle = "#0F0";
+            ctx.font = "50px serif";
+            ctx.fillText(randomMatrixSymbol() , 30 , y);
+        }
+        setInterval(drow,100);
     const randomCharIndex = getRandomInt(chars.length);
-    return chars [randomCharIndex];
-   
+    return chars[randomCharIndex];
+
 }
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
